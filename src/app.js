c7098f6ebae9e69fda1e716cf06110508aee8fac
Jack Durham <jackdurham11@gmail.com>
2018-04-06 13:59:58 +0100

    added depdencies

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import React    from 'react';
+import ReactDOM from 'react-dom';
+import Axios    from 'axios';
+
+class App extends React.Component {
+  render() {
+    constructor() {
+      super();
+
+    }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
